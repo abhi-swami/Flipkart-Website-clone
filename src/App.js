f8d57c18a3e621db687916dd61bf8be59d309632
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-02-24 03:09:25 +0530

    added: Single product page and updated app.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Home from "./Pages/Home/Home.jsx";
 import Footer from "./Components/Footer";
 
 import Navbar from "./Components/Navbar";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <AllRoutes />
       <Home />
       <Footer />
     </div>
