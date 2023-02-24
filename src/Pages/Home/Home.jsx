965f61ab63a6df554aeecdb4f9ac8ec0cfa2c330
sristy verma <sristyel2016@gmail.com>
2023-02-24 08:15:26 +0530

    added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
+import Carousel from '../../Components/Carousel'
 import CategoryBar from "../../Components/CategoryBar/CategoryBar";
+import Header from "../../Components/Header";
 
 import { CategoryBarData } from "../../Datas";
 
@@ -8,11 +9,13 @@ import "./Home.css";
 
 const Home = () => {
   return (
+    <>
     <div className="Home" style={{marginTop:"0px"}}>
       <div className="Home-CategoryContainer" style={{marginTop:"-50px"}}>
         <div className="Home-CategoryBar">
           {CategoryBarData.map((item, index) => (
             <CategoryBar
+           
               key={index}
               Imgsrc={item.imageSrc}
               CategoryName={item.category}
@@ -22,6 +25,9 @@ const Home = () => {
       </div>
      
     </div>
+    <Carousel/>
+    <Header/>
+    </>
   );
 };
 
